b2b9accb2309c1f0819bf14bdc5e8e078f90fc0a
elizabeth lands <elizabeth_lands@hotmail.com>
2023-05-23 10:55:47 +0100

    feat: splash component basic styles for small web

--- src/app/components/splash-banner.tsx
+++ src/app/components/splash-banner.tsx
@@ -4,16 +4,18 @@ import Image from 'next/image';
 
 export default function SplashBanner() {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.container}>
       <h1 className={styles.heading}>Create Change Today.</h1>
       <h2 className={styles.blurb}>Search and discover global charities that align with your passions.</h2>
-      <div className={styles.imageWrapper}>
-        <Image
-          src="/hands-global.jpg"
-          width={500}
-          height={600}
-          alt="helping hands holding the world"
-        />
+      <div className={styles.imageContainer }>
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/hands-global.jpg"
+            alt="helping hands holding the world"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
       </div>
     </div>
   )
