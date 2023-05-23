e6f0b107b9a8b25f36e7dcf98101841cb9041d9d
elizabeth lands <elizabeth_lands@hotmail.com>
2023-05-23 12:47:17 +0100

    feat: make splash responsive

--- src/app/components/splash-banner.tsx
+++ src/app/components/splash-banner.tsx
@@ -6,16 +6,22 @@ export default function SplashBanner() {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Create Change Today.</h1>
-      <h2 className={styles.blurb}>Search and discover global charities that align with your passions.</h2>
-      <div className={styles.imageContainer }>
-        <div className={styles.imageWrapper}>
-          <Image
-            src="/hands-global.jpg"
-            alt="helping hands holding the world"
-            layout="fill"
-            objectFit="contain"
-          />
+
+      <div className={styles.blurbImgWrapper}>
+
+        <h2 className={styles.blurb}>Search and discover global charities that align with your passions.</h2>
+
+        <div className={styles.imageContainer}>
+          <div className={styles.imageWrapper}>
+            <Image
+              src="/hands-global.jpg"
+              alt="helping hands holding the world"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
         </div>
+
       </div>
     </div>
   )
