ccbfe201a4fe6bbf5ef547c3961c21db2273e7fb
elizabeth lands <elizabeth_lands@hotmail.com>
2023-05-23 10:18:06 +0100

    feat: basic font styles and reset wrappers

--- src/app/components/splash-banner.tsx
+++ src/app/components/splash-banner.tsx
@@ -4,15 +4,17 @@ import Image from 'next/image';
 
 export default function SplashBanner() {
   return (
-    <div className={styles.description}>
+    <div className={styles.wrapper}>
       <h1 className={styles.heading}>Create Change Today.</h1>
       <h2 className={styles.blurb}>Search and discover global charities that align with your passions.</h2>
-      <Image
-        src="/hands-global.jpg"
-        width={500}
-        height={600}
-        alt="helping hands holding the world"
-      />
+      <div className={styles.imageWrapper}>
+        <Image
+          src="/hands-global.jpg"
+          width={500}
+          height={600}
+          alt="helping hands holding the world"
+        />
+      </div>
     </div>
   )
 }
